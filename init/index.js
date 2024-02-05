cc0bf26dbed90e9d0d2d5f5cd5fd3c09e653321c
MoxitParmar <moxitparmar2004@gmail.com>
2024-02-05 18:27:01 +0530

    new index.js for send data.js to atlas

--- init/index.js
+++ init/index.js
@@ -1,38 +1,30 @@
-//basic requires
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
+}
 const mongoose = require("mongoose");
-const initdata = require("./data.js")
-const Listing = require("../models/listing.js")
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
 
-//for connect mongoose to our database
-const mongo_url = "mongodb://127.0.0.1:27017/theWeeknd";
-
+// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const DB_URL = process.env.ATLASDB_URL;
 main()
-    .then(() => {
-        console.log("connected to db");
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-
-
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect(mongo_url);
-};
-
+  //   await mongoose.connect(MONGO_URL);
+  await mongoose.connect(DB_URL);
+}
 
 const initDB = async () => {
-
-    //for deleting the previous modified or corrupted data 
-    //for init only the firstly generated sample data
-    await Listing.deleteMany({});
-    //saprating all the objects from our sample data by map mathod
-    initdata.data = initdata.data.map((obj) => ({ ...obj, owner: "659cd5a5428431eac7c8b063" }))
-    //now insert all that objects as document in our listing colletion
-    await Listing.insertMany(initdata.data);
-    console.log("data was initialized");
+  await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "659cd5a5428431eac7c8b063",
+  }));
+  await Listing.insertMany(initData.data);
+  console.log("data was initialized");
 };
-
-//initialis our function
 initDB();
-
