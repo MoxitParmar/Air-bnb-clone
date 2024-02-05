db278a722d6d2ba8d7c8ee1abf68c0d9194eb903
MoxitParmar <moxitparmar2004@gmail.com>
2024-02-05 21:35:24 +0530

    the index.js change

--- init/index.js
+++ init/index.js
@@ -6,18 +6,19 @@ const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
 // const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
-const DB_URL = process.env.ATLASDB_URL;
+const DB_URL = "mongodb+srv://moxit-parmar:bCtcXJNHHZ1eDNtI@cluster0.ludlnin.mongodb.net/?retryWrites=true&w=majority"
 main()
   .then(() => {
     console.log("connected to DB");
   })
-  .catch((err) => console.log(err));
+  .catch((err) => {
+    console.log(err);
+  });
 
 async function main() {
-  //   await mongoose.connect(MONGO_URL);
   await mongoose.connect(DB_URL);
 }
-
+    
 const initDB = async () => {
   await Listing.deleteMany({});
   initData.data = initData.data.map((obj) => ({
@@ -27,4 +28,5 @@ const initDB = async () => {
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
+
 initDB();
